83655c6b9de6efe0a4df3c628a3bccf02eee656a
Jiwon Jeong <jiwon.j.jeong@gmail.com>
2025-02-11 14:59:24 -0800

    Sort and remove duplicates from arr

--- tree.js
+++ tree.js
@@ -4,7 +4,17 @@ const Tree = function(dataArray){
     let root = buildTree(dataArray);
 
     // builds BST and returns root node
-    const buildTree = function(array){
+    const buildTree = function(array){ 
+        array.sort();   // Array.prototype.sort() changes original array
+        removeDuplicatesFromArray(array);
+    }
 
+    const removeDuplicatesFromArray = function(array){
+        for (let i=1; i<array.length; i++){
+            if (array[i] == array[i-1]){
+                array.splice(i, 1);
+                i--;
+            }
+        }
     }
 }
