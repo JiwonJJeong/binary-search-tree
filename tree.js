46f0fed3e656f3262c1d436cbe340a412df61bc4
Jiwon Jeong <jiwon.j.jeong@gmail.com>
2025-02-11 14:48:59 -0800

    Init tree factory

--- tree.js
+++ tree.js
@@ -1,1 +1,10 @@
 // binary search tree factory function
+
+const Tree = function(dataArray){
+    let root = buildTree(dataArray);
+
+    // builds BST and returns root node
+    const buildTree = function(array){
+
+    }
+}
