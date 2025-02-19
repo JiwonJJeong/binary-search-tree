af9ee9b82e3c0a76cfa896c85269fd2d879175e0
Jiwon Jeong <jiwon.j.jeong@gmail.com>
2025-02-18 16:00:05 -0800

    Delete items recurively

--- main.js
+++ main.js
@@ -10,6 +10,7 @@ testTree.prettyPrint();
 testTree.insert(3);
 testTree.insert(2.5);
 testTree.insert(4347);
+testTree.deleteItem(4);
+testTree.deleteItem(6);
 
 testTree.prettyPrint();
-console.log(testTree.root);
--- tree.js
+++ tree.js
@@ -74,12 +74,39 @@ const Tree = function (dataArray) {
     console.error("Caanot insert existing value");
   }
 
-  const deleteItem = function(value){
-    // find node to delete by traversal
-    // if node doesn't have children, just remove that node
-    // if node has one child, just replace node with that child and delete
-    // if node has two children, replace node with order successor and delete
-      // find order successor by minimum of right subtree
+  // recursively searches tree and returns (modified) root (rebuilding it along the way)
+  const deleteItem = function(value, root = this.root){
+    if (root === null){
+      return root;
+    }
+    if (value < root.data){
+      root = deleteItem(value, root.left);
+    } else if (value > root.data){
+      root = deleteItem(value, root.right);
+    } else{
+      // here root.data == value
+      if (root.left == null && root.right ==null){
+        // the node to remove has 0 children
+        return null;
+      } else if (root.left == null){
+        // node to remove has right subtree
+        return root.right;
+      } else if (root.right == null){
+        // node to remove has left subtree
+        return root.left;
+      } else{
+        // node to remove has 2 children
+        let nextSuccessor = root.right; // should be set to min value of right subtree
+        while (nextSuccessor.left != null){
+          nextSuccessor = nextSuccessor.left;
+        }
+        // then replace root's data with successor data
+        root.data = nextSuccessor.data;
+        // and remove old nextSuccessor
+        root.right = deleteItem(nextSuccessor.data, root.right);
+      }
+    }
+    return root;
   }
 
   return {
@@ -87,6 +114,7 @@ const Tree = function (dataArray) {
     prettyPrint,
     root,
     insert,
+    deleteItem,
   };
 };
 
